4e558a211c58383ea6acf186e65f61ed50d81d03
FancyJavaStuff <30594398+FancyJavaStuff@users.noreply.github.com>
2018-03-22 22:06:49 +0100

    Experimentation with StackTraceElements
    TIL:
    Lines from StackTraces can be hidden by compiling with -g
    Without the actual source, using the Class and the Line number will not return the respective line.
    
    e.getMessage returns the invalid value that was given.

--- src/main/java/ch/rs/IssueReported/libhandler/Initializer.java
+++ src/main/java/ch/rs/IssueReported/libhandler/Initializer.java
@@ -4,8 +4,10 @@ package ch.rs.IssueReported.libhandler;
 import ch.rs.IssueReported.credentials.Account;
 import ch.rs.IssueReported.reportGenerator.IssueReport;
 import ch.rs.IssueReported.reporter.ReportingUnitNew;
+import jdk.internal.org.objectweb.asm.ClassReader;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
 
 public class Initializer implements Runnable {
 
@@ -20,9 +22,26 @@ public class Initializer implements Runnable {
         account = new Account("", "");
         ReportingUnitNew rUnit = new ReportingUnitNew(account);
         rUnit.setRepository(owner, repository);
-        rUnit.reportIssueToRepository(new IssueReport());
-        new IssueReport(new IOException());
-        new IssueReport(new NullPointerException());
+        //rUnit.reportIssueToRepository(new IssueReport());
+       // new IssueReport(new IOException());
+        //new IssueReport(new NullPointerException());
+        try{
+            String test[] = {"one","two","three"};
+            System.out.println(test[4]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            String exception[] = e.getClass().getName().split("\\.");
+            String exceptionName = exception[exception.length-1];
+            System.out.println(exceptionName + " was thrown with the cause " + e.getMessage());
+            for(StackTraceElement ste : e.getStackTrace()){
+                System.out.println("Method name: " + ste.getMethodName());
+                System.out.println("Class name: " + ste.getClassName());
+                System.out.println("File name: " + ste.getFileName());
+                System.out.println("at line: " + ste.getLineNumber());
+                System.out.println("Is native method: " + ste.isNativeMethod());
+                System.out.println(ste.toString());
+                System.out.println("-----------------------------");
+            }
+        }
 
     }
 }
